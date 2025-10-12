99a6f1a5408d3a48797a5c96cb6b9207468a66a6
AbhayKatoch <abhaykatoch210@gmail.com>
2025-10-12 12:49:13 +0530

    commit

--- core/settings.py
+++ core/settings.py
@@ -57,7 +57,11 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework_simplejwt.authentication.JWTAuthentication",
-    ]
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ],
 }
 
 
